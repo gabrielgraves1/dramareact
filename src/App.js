8e0237c42b85f319f1db760eca6e6694dc4ab098
me <improvisaciontotal@gmail.com>
2021-05-10 19:23:51 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+<div id="arriba">
+        <div className="logoSuperior">
+            <img src="maquinola.jpg" alt="logo"/>
+            <a href="#main"></a>
+        </div>
+
+
+        <div className="links-arriba">
+            <a href="#bio">Bio</a>
+            <a href="#ofertaCursos">Oferta de cursos</a>
+            <a href="#materiales">Materiales</a>
+            <a href="#contacto">Contacto</a>
+        </div>
     </div>
+    <div className="titulo">
+        <h1>Lorem, ipsum.</h1>
+    </div>
+    <div id="core"/>
+    <div className="cursos">
+        <div className="curso1">
+            <img src="https://images.pexels.com/photos/34587/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+            <h3>Lorem ipsum dolor sit.</h3>
+            <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ea?</h4>
+            <button><a href="#">Más info</a></button>
+    </div>
+    </div>
+    <div className="curso2">
+            <img src="https://images.pexels.com/photos/373465/pexels-photo-373465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+            <h3>Lorem ipsum dolor sit.</h3>
+            <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ea?</h4>
+            <button><a href="#">Más info</a></button>
+        </div>
+            <div className="curso3">
+            <img src="https://images.pexels.com/photos/373465/pexels-photo-373465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+            <h3>Lorem ipsum dolor sit.</h3>
+            <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ea?</h4>
+            <button><a href="#">Más info</a></button>
+        </div>
+  
+        <div id="bottom">
+        <div className="bioRecursos">
+        <div className="Bio">
+            <img src="maquinola.jpg" alt=""/>
+
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere ad quasi minima itaque numquam aperiam explicabo quas eum illum ducimus.</h3>
+            <button><a href="#">Más info</a></button>
+
+
+        <div className="recursos">
+            <button><a href="#">Más info</a></button>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum!</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error maxime in voluptates.
+            </h3>
+            <img src="herramientas.jpeg" alt=""/>
+        </div>
+        </div>
+        </div>
+        </div>
+    
+        <div id="footer">
+        <div className="contacto">
+            <h2>Vías de contacto</h2>
+            <div className="redes">
+                <div className="email">
+                <img src="email.png" alt=""/><a mailto="xxx@xxx.x"/>
+                </div>
+                <div className="red1">
+                <img src="twitter.png"/> <a href="https://twitter.com/home"/>
+                </div>
+                <div className="red2">
+                <img src="instagram.png" alt=""/>
+                </div>
+                <div className="red3">
+                <img src="facebook.png" alt=""/>
+        </div>
+        </div>
+        </div>
+        </div>
+        <div className="formulario">
+            <h2>Quiero saber más</h2>
+            <form/>
+                <input type="email" placeholder="Ingresá tu email" required/>
+                <br></br>    
+                <textarea name="" id="" cols="70" rows="10" placeholder="Ingresá tu mensaje"></textarea>
+                <br></br>
+                <button>Enviar</button>
+</div>
+</>
   );
 }
 
+export default App;
 export default App;
